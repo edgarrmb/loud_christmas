cd97b2ba8d2b2e3e8c1aef7af179440ec7053ad0
Edgar <milianbrionesedgarramon@gmail.com>
2022-11-26 23:22:52 -0600

    UwU

--- usuarios/usuarios.php
+++ usuarios/usuarios.php
@@ -1,17 +1,3 @@
-<?php
-
-include 'config.php';
-
-session_start();
-
-$admin_id = $_SESSION['admin_id'];
-
-if(!isset($admin_id)){
-   header('location:login.php');
-}
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -27,40 +13,30 @@ if(!isset($admin_id)){
     
     <title>Usuarios</title>
 </head>
-<body>
+<body>                
 
     <h1>nuestra sección de equipo</h1>
 
     <section class="container">
-    <?php
-        $link = mysql_connect("localhost", "root");
-        mysql_select_db("loud_christmas", $link);
-        $result = mysql_query("SELECT * from usuarios ORDER BY id", $link);
-        // comienza un bucle que leera todos los registros existentes en la tabla
-        while($row = mysql_fetch_array($result)){
-            //$row es un array con todos los campos existentes en la tabla
-            
-        } //fin del bucle de instrucciones
-        mysql_free_result($result); //Liberamos los registros
-        mysql_close($link); //cerramos la conexion con la base de datos
-    ?>
-
-    <?php
-      $select_profile = $conn->prepare("SELECT * FROM `usuarios` WHERE id = ?");
-      $select_profile->execute([$admin_id]);
-      $fetch_profile = $select_profile->fetch(PDO::FETCH_ASSOC);
-    ?>
+        <?php
+            $link = mysql_connect("localhost", "root");
+            mysql_select_db("loud_christmas", $link);
+            $result = mysql_query("SELECT * from usuarios ORDER BY id", $link);
+            // comienza un bucle que leera todos los registros existentes en la tabla
+            while($row = mysql_fetch_array($result)){
+                //$row es un array con todos los campos existentes en la tabla
+        ?>
 
         <div class="card">
             <div class="bg-image">
                 <img src="img/bg-image.jpg" alt="">
             </div>
             <div class="pic">
-                <img src="uploaded_img/<?= $fetch_profile['image']; ?>" alt="">
+                <img src="uploaded_img/<?= $row['image']; ?>" alt="">
             </div>
             <div class="info">
-                <h3><?= $fetch_profile['name']; ?></h3>
-                <span><i class="fas fa-code"></i> Diseñador web </span>
+                <h3><?= $row['name']; ?></h3>
+                <span><i class="fas fa-code"></i> <?= $row['user_type']; ?></span>
                 <p>Si el cliente es el cliente, el cliente podrá recibir el servicio de atención al cliente. ¡Que huya menos de las necesidades de la vida, y el que odia los tiempos, que los merezca más penas!</p>
                 <div class="icons">
                     <a href="#" class="fab fa-facebook-f"></a>
@@ -71,7 +47,12 @@ if(!isset($admin_id)){
 
             </div>
         </div>
-    
+
+        <?php
+            } //fin del bucle de instrucciones
+            mysql_free_result($result); //Liberamos los registros
+            mysql_close($link); //cerramos la conexion con la base de datos
+        ?>
 
     </section>
     
