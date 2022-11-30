161fb02d06747fb8336dfdd1fd55d9af45dd2d5f
Edgar <milianbrionesedgarramon@gmail.com>
2022-11-30 16:25:17 -0600

    UwU

--- index.php
+++ index.php
@@ -5,10 +5,84 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Loud Christmas</title>
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/style4.css">
+
 </head>
 <body>
 
-<a href="usuarios/login.php">USUARIO</a><a href="jugadores/login_form.php">JUGADOR</a>
+<!-- custom cursors  -->
+<div class="cursor-1"></div>
+<div class="cursor-2"></div>
+
+
+<div id="menu-bars" class="fas fa-bars"></div>
+    
+<!-- header section starts  -->
+<header>
+
+    <a href="#" class="logo"> <span>Loud</span> Christmas </a>
+
+    <nav class="navbar">
+        <a href="#user">Usuario</a>
+        <a href="#player">Jugador</a>
+    </nav>
+
+    <div class="follow">
+        <a href="#" class="fab fa-facebook-f"></a>
+        <a href="#" class="fab fa-twitter"></a>
+        <a href="#" class="fab fa-instagram"></a>
+        <a href="#" class="fab fa-github"></a>
+    </div>
+
+</header>
+<!-- header section ends -->
+
+<!-- home section starts  -->
+<section class="home" id="user">
+
+    <div class="content">
+        <span class="hi"> Hola... </span>
+        <h3> Soy <span> Usuario </span> </h3>
+        <p class="info"> ¡Registrate! </p>
+        <p class="text"> Es muy doloroso, pero es fácil perder peso. Nació para recibir algo más. Otros, ¿qué? ¡Que sean cegados por el dolor! </p>
+        <a href="usuarios/login.php" class="btn">Continuar</a>
+    </div>
+
+    <div class="image">
+        <img src="images/home-img.jpg" alt="">
+    </div>
+
+</section>
+<!-- home section ends -->
+
+<!-- home section starts  -->
+<section class="home" id="player">
+    <div class="image">
+        <img src="images/home-img-2.jpg" alt="">
+    </div>
+
+    <div class="content">
+        <span class="hi"> Hola... </span>
+        <h3> Soy <span> Jugador </span> </h3>
+        <p class="info"> ¡Registrate! </p>
+        <p class="text"> Es muy doloroso, pero es fácil perder peso. Nació para recibir algo más. Otros, ¿qué? ¡Que sean cegados por el dolor! </p>
+        <a href="jugadores/login_form.php" class="btn">Continuar</a>
+    </div>
+
+
+</section>
+<!-- home section ends -->
+
+<!-- footer section  -->
+<footer class="footer"> Creado por <span> Edgar Milian </span> | all rights reserved! </footer>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
 
 </body>
-</html>  
+</html>
